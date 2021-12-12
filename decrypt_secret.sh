5675f0272f85d5f58a91012716d4339db823efbe
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-12-12 21:40:30 +0000

    UPDATE - Configure Nginx on remote server with Ansible via CICD-Githubactions

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,6 +4,6 @@
 mkdir ~/.ssh
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-echo "${secrets.SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
+echo "$secrets.SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 # gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" --output $HOME/secrets/ta-lab-key.pem ta-lab-key.pem.gpg
